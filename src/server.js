5ce8dbe71ee6f168d4c6fd585bfb94fefed1deff
alissonjosef <alissonjosef@gmail.com>
2019-10-21 17:39:55 -0300

    v0.1

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+// GET, POST, PUT, DELETE
+
+// req.query = acessar query pares (para filtros)
+// req.params - Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
