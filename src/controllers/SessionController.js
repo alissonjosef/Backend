00e12a3cf4790f345687ff32b828eb9691dd7ebf
alissonjosef <alissonjosef@gmail.com>
2019-10-22 10:07:25 -0300

    V0.2

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,5 +1,18 @@
 // index, show, store, update, destroy
+const User = require('../models/User');
 
 module.exports = {
-    index()
+    async store(req, res){
+        const { email } = req.body;
+
+        let user = await User.findOne({ email: email });
+
+        if (!user){
+            user = await User.create({ email });
+        }
+
+        //const user = await User.create({ email });
+
+        return res.json(user);
+    }
 };
