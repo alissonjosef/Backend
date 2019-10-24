8667a7a506c51ebcd5e78579ddb97181dabe7baf
alissonjosef <alissonjosef@gmail.com>
2019-10-24 14:17:53 -0300

    V0.7

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://alissonsilva:alissonsilva@cluster0-qhbwo.mongodb
 // req.params - Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
